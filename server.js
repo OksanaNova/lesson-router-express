ac9fff0aeda1f186452993cd5586cbf51bda400f
OksanaNova <o.nova0804@gmail.com>
2025-06-23 10:33:27 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(4000, () => {
+    console.log(`It's working`)
+})
+
+// POSTS
+// app.get('/posts', (req, res) => {
+//     res.send('GET POSTS')
+// })
+
+// app.post('/posts', (req, res) => {
+//     res.send('POST MY POSTS')
+// })
+
+// app.put('/posts', (req, res) => {
+//     res.send('PUT MY POSTS')
+// })
+
+// app.delete('/posts', (req, res) => {
+//     res.send('DELETE MY POSTS')
+// })
+
+// COMMENTS
+// app.get('/comments', (req, res) => {
+//     res.send('GET COMMENTS')
+// })
+
+// app.post('/comments', (req, res) => {
+//     res.send('POST MY COMMENTS')
+// })
+
+// app.put('/comments', (req, res) => {
+//     res.send('PUT MY COMMENTS')
+// })
+
+// app.delete('/comments', (req, res) => {
+//     res.send('DELETE MY COMMENTS')
+// })
+
+// LIKES
+// app.get('/likes', (req, res) => {
+//     res.send('GET LIKES')
+// })
+
+// app.post('/likes', (req, res) => {
+//     res.send('POST MY LIKES')
+// })
+
+// app.put('/likes', (req, res) => {
+//     res.send('PUT MY LIKES')
+// })
+
+// app.delete('/likes', (req, res) => {
+//     res.send('DELETE MY LIKES')
+// })
+
+
+
